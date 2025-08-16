47526e98fd9befd53b89fd57fc9c9132a286949f
Landon Noss <landongn@gmail.com>
2025-08-16 15:58:31 -0700

    auth system

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :more, :scopes,
+  user: [
+    default: true,
+    module: More.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: More.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :more,
   ecto_repos: [More.Repo],
   generators: [timestamp_type: :utc_datetime]
